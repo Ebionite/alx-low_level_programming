7d1b9ed85f4fbe77d36e09a32a2e0f6df0a8b130
Ebionite <ebichibuzoac@gmail.com>
2023-06-16 15:20:23 -0700

    My task the variables if, else and while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - program that prints all possible different combinations of 2 digits
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
  * Return: 0
  */
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
+	int c;
+	int d;
+	int e = 0;
 
-	while (c <= 99)
+	while (e < 10)
 	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-
-		if (f_d < l_d)
+		d = 0;
+		while (d < 10)
 		{
-			putchar(f_d);
-			putchar(l_d);
-
-			if (c != 89)
+			c = 0;
+			while (c < 10)
 			{
-				putchar(',');
-				putchar(' ');
+				if (c != d && d != e && e < d && d < c)
+				{
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
+
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+
+				c++;
 			}
+			d++;
 		}
-		c++;
+		e++;
 	}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
 
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 /**
- * main - print if the number is postive, zero, or negative
- *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
  * Return: 0
  */
 int main(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-	putchar(c);
-}
-putchar('\n');
-return (0);
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
 }
