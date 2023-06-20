a5106e556b31af5905b89d62f119cb5897f6df4e
Ebionite <ebichibuzoac@gmail.com>
2023-06-20 15:08:35 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,9 @@
 /**
  * main - Prints _putchar followed by a newline
  * Return: Always 0.
- */int main(void)
+ */
+
+int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
