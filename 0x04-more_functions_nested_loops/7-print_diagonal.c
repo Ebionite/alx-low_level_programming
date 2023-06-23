f74387fddc71be8749627a2b81b3306e7117683b
Ebionite <ebichibuzoac@gmail.com>
2023-06-23 20:39:59 +0100

    The functions of the nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,30 @@
 #include "main.h"
 
 /**
- * print_diagonal - check for a digit
- * @n : number of \\ to be printed
- * Return:void
+ * print_diagonal - Draws a diagonal lines according parameter
+ * @n: The number of times to print diagonal lines
+ *
+ * Return: empty
  */
-
 void print_diagonal(int n)
 {
-	int i = 0, ii;
-	while (i < n && n > 0)
-	{
-		ii = 0;
-		while (ii < i)
-		{
-			_putchar(' ');
-			ii++;
-		}	
-		_putchar('\\');
-		_putchar('\n');
-		i++;
-	}
-	if (i == 0)
-	_putchar('\n');
+	int x, y;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}
+
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
 }
