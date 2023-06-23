f2faaf91aa5c9ef2881a11776e45eee5d38357c3
Ebionite <ebichibuzoac@gmail.com>
2023-06-23 21:25:12 +0100

    The functions of the nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,34 +1,28 @@
 #include "main.h"
-
 #include <stdio.h>
-#include <stdlib.h>
-
-int _putchar(char c);
 
 /**
- * print_number - prints numbers
- * @n: number to be printed
- * Return:void
+ * print_number - Prints an integer.
+ * @n: The integer to prints.
+ *
+ * Return: Nothing!
  */
-
 void print_number(int n)
 {
-	unsigned int n1 = 0;
-	if  (n < 0)
+	unsigned int k = n;
+
+	if (n < 0)
 	{
-		n1 = -n;
+		n *= -1;
+		k = n;
 		_putchar('-');
 	}
 
-	else
-	{
-		n1 = n;
-	}
+	k /= 10;
 
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
+	if (k != 0)
+		print_number(k);
 
-	_putchar((n1 % 10) + '0');
+	_putchar((unsigned int) n % 10 + '0');
+
 }
