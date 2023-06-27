d42885f51824145622076cecec334c3f304d5e0c
Ebionite <ebichibuzoac@gmail.com>
2023-06-27 20:47:42 +0100

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,15 @@
 #include "main.h"
-/**
- * _strcmp - function that compares two strings
- * @s2: paramater for string 2
- * @s1: paramater for string 1
- * Return: Always 0.
- */
-int _strcmp(char *s1, char *s2)
-{
-	int a = 0;
 
-	while (s1[a] != '\0')
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str)
 	{
-		if (s1[a] < s2[a])
-			return (s1[a] - s2[a]);
-		if (s1[a] > s2[a])
-			return (s1[a] - s2[a]);
-		a++;
+		_putchar(*str);
+		str++;
 	}
-	return (0);
+	_putchar('\n');
 }
