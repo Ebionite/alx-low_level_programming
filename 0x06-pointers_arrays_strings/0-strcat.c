59e860ea609d96026f68ec92f2be53a73b2302c9
Ebionite <ebichibuzoac@gmail.com>
2023-07-02 17:30:33 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 
 /**
- * _strcat - Concatenates two strings
- * @dest: The destination string
- * @src: The source string
- *
- * Return: A pointer to the resulting string dest
- */
+  * _strcat - Concatenates two strings
+  * @dest: The destination string
+  * @src: The source string
+  *
+  * Return: A pointer to the resulting string dest
+  */
 char *_strcat(char *dest, char *src)
-	int dlen = 0, i;
 {
+	int dlen = 0, i;
+
 	while (dest[dlen])
 	{
 		dlen++;
