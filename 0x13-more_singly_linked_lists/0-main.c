38893c5a3d67b3b9d93d6beb5d4aae2904c09856
Ebionite <ebichibuzoac@gmail.com>
2023-08-01 15:15:11 -0700

    Updated task 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-main.c
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -18,15 +18,15 @@ int main(void)
 	head = &hello;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-{
-	printf("Error\n");
-	return (1);
-}
-new->n = 9;
-new->next = head;
-head = new;
-n = print_listint(head);
-printf("-> %lu elements\n", n);
-free(new);
-return (0);
+	{
+		printf("Error\n");
+		return (1);
+	}
+	new->n = 9;
+	new->next = head;
+	head = new;
+	n = print_listint(head);
+	printf("-> %lu elements\n", n);
+	free(new);
+	return (0);
 }
